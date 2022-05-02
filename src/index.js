d635a7f9f5919aa168d786f9f24de7d1b154b36f
pnauj <torres.jpw@gmail.com>
2022-05-02 16:57:57 -0300

    se agregaron rutas

--- src/index.js
+++ src/index.js
@@ -1,4 +1,33 @@
-import express from 'express';
+import express from "express";
+import morgan from "morgan";
+import cors from "cors"; 
 
 
-console.log('desde el backend')
+//creo una constancia de express
+const app = express();
+
+//crear un puerto
+app.set("port", process.env.PORT || 4000);
+
+app.listen(app.get("port"), () => {
+  console.log("estoy en el puerto " + app.get("port"));
+});
+
+//middlewares o configuraciones extras
+app.use(morgan('dev')); // informacion extra en la terminal
+app.use(cors()); // permite recibir peticiones externas
+
+
+//ruta de prueba
+app.get('/products', (req, res)=>{
+  //lo que quiero que pase cuando se ejecute la consulta
+  res.send('hola desde el  backend')
+})
+app.get('/', (req, res)=>{
+  //lo que quiero que pase cuando se ejecute la consulta
+  res.send('hola desde la ruta principal')
+})
+
+
+// clase 09 de febrero
+// tiempo 1:36
